1633ace5a271047f95b07594b80ad535515448ed
Jan Mahbub Milon <jan.milon.56@gmail.com>
2025-05-26 19:23:10 +0600

    Add homepage view and implement counter bloc structure

---
+++ lib/homepage/presentation/homepage_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomepageView extends StatelessWidget {
+  const HomepageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homepage'),
+      ),
+      body: Center(
+        child: const Text('Welcome to the Homepage!'),
+      ),
+    );
+  }
+}
